097252034619aa5e4584b189ea66ee83dabf5254
Mohadese-Keshtkar <mohadesekeshtkar4@gmail.com>
2023-10-13 14:51:31 +0330

    Update style css and add js future

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
-// // select car year
-// let year = document.querySelector('#years')
-
-// // Add the last twenty years
-// function addNewYears() {
-//     let nowyear = 1402
-//     for (let i = 0; i <= 20; i++) {
-//         nowyear -= 1
-//         year.insertAdjacentHTML("beforeend", `
-//         <option>${nowyear}</option>
-
-//         `)
-//     }
-// }
-// addNewYears() //call function
-
-
-
+// selected Form
 const form = document.querySelector(".contentList")
+
+// this Event for better loadeing js code...
 document.addEventListener("DOMContentLoaded", afterLoad)
+
+// submit function for  Car Insurance form  
 document.addEventListener("submit", submitForm)
 
 
@@ -25,15 +12,99 @@ function submitForm(e) {
     e.preventDefault() // Prevent page refresh
 
     const make = document.querySelector("#boxOptions").value
-    const years = document.querySelector("#years").value
+    const year = document.querySelector("#years").value
     const level = document.querySelector("input[name='insurance']:checked")
 
     if (make === "" || years === "" || level === "") {
-        alert('مدل خودرو را انتخاب کنید❌')
+        displayMsg('مدل خودرو را انتخاب کنید')
     } else {
         alert('ثبت شد✅')
+
+        let insuranceCase = {
+            make: make,
+            year: year,
+            level: level
+
+        }
     }
 }
+
+
+// Price calculation function
+function calculaterPrice(info) {
+    let price = 0, base = 2000000
+    const make = info.make
+
+    switch (make) {
+        case "1":
+            price = base * 1.15
+            break
+        case "2":
+            price = base * 1.30
+            break
+        case "3":
+            price = base * 1.80
+            break
+
+    }
+
+
+
+    // Calculate Year
+    // get the year
+    const
+        year = info.year,
+        // diffrence = getYearDiffrence(year)
+        diffrence = function (year) {
+            // Convert to number
+            let
+                persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
+                arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
+                fixNumbers = function (str) {
+                    if (typeof str === 'string') {
+                        for (var i = 0; i < 10; i++) {
+                            str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+                        }
+                    }
+                    return parseInt(str);
+                };
+
+            // get max year
+            const now = new Date().toLocaleDateString('fa-IR')
+            let nowYear = now.slice(0, 4)
+            let max = fixNumbers(nowYear)
+            year = max - year
+
+            return year
+        }
+
+    // 3% cheaper for each year
+    price = price - ((diffrence * 3) / 100) * price
+
+}
+
+
+// funstion display message
+function displayMsg(msg) {
+
+    // create new div for error
+    const messageBox = document.createElement("div")
+
+    //add classlist to html codes
+    messageBox.classList = "error"
+
+    messageBox.innerText = msg
+
+    form.insertBefore(messageBox, document.querySelector(".CarModel"))
+
+    setTimeout(() => {
+        document.querySelector(".error").remove()
+    }, 5000)
+
+
+}
+
+
 
 function afterLoad() {
     yearsFunction()
